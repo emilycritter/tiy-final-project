95ccff1d23c7dfdcf694cf90fc9b4641715c066e
emily.c.ritter <emily.c.ritter@gmail.com>
2016-04-07 12:16:06 -0500

    Set up artists, pieces, and users api controllers. Create files for json (have not added any content)

--- app/controllers/api/artists_controller.rb
+++ app/controllers/api/artists_controller.rb
@@ -1,2 +1,58 @@
+require 'open-uri'
+
 class Api::ArtistsController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @artists = Artist.all.order("shop_name asc")
+  end
+
+  def show
+    @artist = Artist.find_by id: params[:id]
+  end
+
+  def create
+    @artist = Artist.new artist_params
+
+    url = params[:artist][:photo_url]
+    if url.present?
+      open(url, "rb") do |file|
+        @artist.photo = file
+      end
+    end
+
+    if @artist.save
+      render :show
+    else
+      render json: {errors: @artist.errors}, status: 422
+    end
+
+  end
+
+  def update
+    @artist = Artist.find_by id: params[:id]
+
+    url = params[:artist][:photo_url]
+    if url.present?
+      open(url, "rb") do |file|
+        @artist.photo = file
+      end
+    end
+
+    if @artist.update artist_params
+      render :show
+    else
+      render json: {errors: @artist.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @artist = Artist.find_by id: params[:id]
+    @artist.destroy
+    head :ok
+  end
+
+  def artist_params
+    params.require(:artist).permit(:shop_name, :photo, :bio, :user_id, :photo)
+  end
 end
--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,46 @@
 class Api::UsersController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def index
+    @users = User.all.order("full_name asc")
+  end
+
+  def show
+    @user = User.find_by id: params[:id]
+  end
+
+  def me
+    @user = User.find_by id: @current_user.id
+  end
+
+  def create
+    @user = User.new user_params
+
+    if @user.save
+      render :show
+    else
+      render json: {errors: @user.errors}, status: 422
+    end
+
+  end
+
+  def update
+    @user = User.find_by id: params[:id]
+
+    if @user.update user_params
+      render :show
+    else
+      render json: {errors: @user.errors}, status: 422
+    end
+  end
+
+  def destroy
+    @user = User.find_by id: params[:id]
+    @user.destroy
+    head :ok
+  end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :phone_number)
+  end
 end
