c7da480d1dbfb9560e8e99fc377c61ec75554b21
emily.c.ritter <emily.c.ritter@gmail.com>
2016-04-11 15:55:41 -0500

    Remove unnecessary tests

--- test/models/order_item_test.rb
+++ test/models/order_item_test.rb
@@ -2,10 +2,8 @@ require "test_helper"
 
 class OrderItemTest < ActiveSupport::TestCase
   def order_item
-    @order_item ||= OrderItem.new
   end
 
   def test_valid
-    assert order_item.valid?
   end
 end
--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -2,10 +2,8 @@ require "test_helper"
 
 class OrderTest < ActiveSupport::TestCase
   def order
-    @order ||= Order.new
   end
 
   def test_valid
-    assert order.valid?
   end
 end
