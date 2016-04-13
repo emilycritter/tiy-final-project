fbf8ed8f77e037ee96ac4bb0d7fe12f8946890e8
emily.c.ritter <emily.c.ritter@gmail.com>
2016-04-13 16:51:58 -0500

    Start map on current location

--- app/assets/javascripts/components/map_display.js.jsx
+++ app/assets/javascripts/components/map_display.js.jsx
@@ -10,7 +10,7 @@ var MapDisplay = React.createClass({
 
   componentDidMount(){
     var component = this;
-    var mymap = L.map('mapid').setView([29.73, -95.39], 13);
+    var mymap = L.map('mapid');
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
@@ -18,12 +18,6 @@ var MapDisplay = React.createClass({
       id: 'emilycritter.pia1bd72',
       accessToken: 'pk.eyJ1IjoiZW1pbHljcml0dGVyIiwiYSI6ImNpbWdmZXhmYzAyMDV1NGx2bHM0MTNzNGYifQ.7KYzLItfXBfg5Zs-757BGw'
     }).addTo(mymap);
-
-    this.state.artists.map(function(artist){
-      L.marker([artist.latitude, artist.longitude]).addTo(mymap)
-        // .bindPopup('<b>' + artist.shop_name+ '</b>' + artist.location + '<br/><img src="' + artist.photo_url + '"/>' + '<br/><a href="' + artist.artist_url + '">View Shop</a>');
-        .bindPopup('<b>' + artist.shop_name+ '</b>' + artist.location);
-    });
 
     mymap.locate({setView: true, maxZoom: 13});
 
@@ -45,6 +39,12 @@ var MapDisplay = React.createClass({
     }
 
     mymap.on('locationerror', onLocationError);
+
+    this.state.artists.map(function(artist){
+      L.marker([artist.latitude, artist.longitude]).addTo(mymap)
+        // .bindPopup('<b>' + artist.shop_name+ '</b>' + artist.location + '<br/><img src="' + artist.photo_url + '"/>' + '<br/><a href="' + artist.artist_url + '">View Shop</a>');
+        .bindPopup('<b>' + artist.shop_name+ '</b>' + artist.location);
+    });
 
   },
 
