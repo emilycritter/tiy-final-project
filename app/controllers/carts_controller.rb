d1be123097ae52bfb64e27687ede2b602d3c66fe
emily.c.ritter <emily.c.ritter@gmail.com>
2016-04-13 08:55:09 -0500

    Add conditionals for @current_user

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,6 +1,10 @@
 class CartsController < ApplicationController
   def view
-    @order = Order.find_by status: 'cart', user_id: @current_user.id
+    if @current_user
+      @order = Order.find_by status: 'cart', user_id: @current_user.id
+    else
+      redirect_to root_path
+    end
   end
 
   def add_to_cart
