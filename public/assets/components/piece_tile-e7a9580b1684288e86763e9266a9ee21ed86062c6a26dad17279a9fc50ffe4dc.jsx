e5b5a9e1e86dfaf331d1b5d35e167e3c5b635c6f
emily.c.ritter <emily.c.ritter@gmail.com>
2016-04-18 09:21:57 -0500

    Add viewport

---
+++ public/assets/components/piece_tile-e7a9580b1684288e86763e9266a9ee21ed86062c6a26dad17279a9fc50ffe4dc.jsx
@@ -0,0 +1,38 @@
+var PieceTile = React.createClass({
+  componentWillMount(){
+    $('.price').css('display', 'none');
+  },
+  componentDidMount(){
+    $('.price').css('display', 'block');
+  },
+
+  render(){
+
+    var piece = this.props.piece;
+
+    var pieceTitleTruncate = function(title){
+      if (title.length > 60) {
+        return title.substring(0,60)+'...'
+      } else {
+        return title
+      }
+    };
+
+    return <div className="flip-container">
+      <a href={piece.piece_url}>
+        <div className="card img-responsive" id="{piece.id}">
+          <div className="front">
+              <img src={piece.photo_url} className="img-responsive my-img" />
+              <div className="price">{piece.price_formatted}</div>
+          </div>
+          <div className="back">
+              <h4>{pieceTitleTruncate(piece.title)}</h4>
+              <h5>{piece.artist.shop_name}</h5>
+              <h5>{piece.price_formatted}</h5>
+          </div>
+        </div>
+      </a>
+    </div>
+
+  }
+});
